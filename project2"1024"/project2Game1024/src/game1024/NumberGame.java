9a1da5b5bd8ee7491cfbc57400a45389c4fa8555
Jacob Walton <waltojac@mail.gvsu.edu>
2017-02-06 12:03:25 -0500

    initial code

---
+++ project2"1024"/project2Game1024/src/game1024/NumberGame.java
@@ -0,0 +1,55 @@
+package game1024;
+
+import java.util.ArrayList;
+
+public class NumberGame implements NumberSlider {
+
+	@Override
+	public void resizeBoard(int height, int width, int winningValue) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setValues(int[][] ref) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Cell placeRandomValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean slide(SlideDirection dir) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<Cell> getNonEmptyTiles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public GameStatus getStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void undo() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
